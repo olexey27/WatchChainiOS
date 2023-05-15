a0490c16fd04c6691c9b5980119b32831a009199
olexey27 <allxyog@gmail.com>
2023-05-15 20:26:25 +0200

    new Crypto View

--- WatchChainIOS/Cell/CoinCell.swift
+++ WatchChainIOS/Cell/CoinCell.swift
@@ -33,7 +33,23 @@ class CoinCell: UITableViewCell {
         label.text = "Error"
         return label
     }()
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.setupUI()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public func configure(with coin: Coin) {
+        self.coin = coin
+        self.coinName.text = coin.name
+    }
+    
+    
+    // MARK: - UI Setup
     private func setupUI() {
         self.addSubview(coinLogo)
         self.addSubview(coinName)
--- WatchChainIOS/Models/CryptoModels/Coin.swift
+++ WatchChainIOS/Models/CryptoModels/Coin.swift
@@ -27,7 +27,7 @@ struct Coin {
 
 extension Coin {
     
-    public func getMockArray() -> [Coin] {
+    public static func getMockArray() -> [Coin] {
         return [
             Coin(id:1, name: "Bitcoin", max_supply: 200, cmc_rank: 1, quote: Quote(CAD: Quote.CAD(price: 50000, market_cap: 1_000_000))),
             Coin(id:2, name: "Ethereum", max_supply: nil, cmc_rank: 2, quote: Quote(CAD: Quote.CAD(price: 2000, market_cap: 500_000))),
