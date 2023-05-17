9e599069a9a6468b8cb08e1f9aacb3f627295ae0
olexey27 <allxyog@gmail.com>
2023-05-17 12:03:51 +0200

    CoinError + API works

---
+++ WatchChainIOS/Models/CryptoModels/CoinError.swift
@@ -0,0 +1,27 @@
+//
+//  CoinError.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 17.05.23.
+//
+
+import Foundation
+
+struct CoinError: Decodable {
+    let errorCode: Int
+    let errorMessage: String
+    
+    enum CodingKeys: String, CodingKey {
+        case status
+        case errorCode = "error_code"
+        case errorMessage = "error_message"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let status = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .status)
+        
+        errorCode = try status.decode(Int.self, forKey: .errorCode)
+        errorMessage = try status.decode(String.self, forKey: .errorMessage)
+    }
+}
