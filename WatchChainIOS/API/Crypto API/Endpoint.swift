0aa98179cb3ef54ac6d5e9c30c9a9478713ded11
olexey27 <allxyog@gmail.com>
2023-05-22 22:11:25 +0200

    Fetching & Displaying API Data

--- WatchChainIOS/API/Crypto API/Endpoint.swift
+++ WatchChainIOS/API/Crypto API/Endpoint.swift
@@ -9,7 +9,17 @@ import Foundation
 
 enum Endpoint {
     
-    case fetchCoins(url: String = "/v1/cryptocurrency/listings/latest", currency: String = "CAD")
+    case fetchCoins(url: String = "/v1/cryptocurrency/listings/latest")
+    
+    var request: URLRequest? {
+    
+        guard let url = self.url else { return nil }
+        var request = URLRequest(url: url)
+        request.httpMethod = self.httpMethod
+        request.httpBody = self.httpBody
+        request.addValues(for: self)
+        return request
+    }
     
     private var url: URL? {
         var components = URLComponents()
@@ -17,24 +27,24 @@ enum Endpoint {
         components.host = Constants.baseURL
         components.port = Constants.port
         components.path = self.path
-        components.queryItems = []
+        components.queryItems = self.queryItems
         return components.url
     }
     
     private var path: String {
         switch self {
-        case .fetchCoins(let url, _):
+        case .fetchCoins(let url):
             return url
         }
     }
     
-    private var queryItem: [URLQueryItem] {
+    private var queryItems: [URLQueryItem] {
         switch self {
-        case .fetchCoins(_, let currency):
+        case .fetchCoins:
             return [
             URLQueryItem(name: "limit", value: "150"),
             URLQueryItem(name: "sort", value: "market_cap"),
-            URLQueryItem(name: "convert", value: currency),
+            URLQueryItem(name: "convert", value: "CAD"),
             URLQueryItem(name: "aux", value: "cmc_rank,max_supply,circulating_supply,total_supply")
             ]
         }
@@ -51,6 +61,17 @@ enum Endpoint {
         switch self {
         case .fetchCoins:
             return nil
+        }
+    }
+}
+
+extension URLRequest {
+    mutating func addValues(for endpoint: Endpoint) {
+        switch endpoint {
+        case .fetchCoins:
+            self.setValue(HTTP.Headers.Value.applicationJson.rawValue, forHTTPHeaderField: HTTP.Headers.Key.contentType.rawValue)
+            
+            self.setValue(Constants.API_KEY, forHTTPHeaderField: HTTP.Headers.Key.apiKey.rawValue)
         }
     }
 }
