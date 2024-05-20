5091cb4cfd7923f4bdcb859ce45f456429c1a601
olexey27 <allxyog@gmail.com>
2024-05-20 23:19:59 +0200

    Quiz func add

---
+++ WatchChainIOS/Quiz/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 20.05.24.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
