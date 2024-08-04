b501b32bb52b2b54f4d8928afd8f6ca4ca0ce381
olexey27 <allxyog@gmail.com>
2024-08-05 00:10:11 +0200

    Message Feature

---
+++ WatchChainIOS/Controllers/Firebase/FirebaseConstants/FirebaseConstants.swift
@@ -0,0 +1,8 @@
+//
+//  FirebaseConstants.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 04/08/2024.
+//
+
+import Foundation
---
+++ WatchChainIOS/Models/Firebase/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 04/08/2024.
+//
+
+import Foundation
