4baa6aefcb5659f6e060d7fdfe50e97782b414c3
olexey27 <allxyog@gmail.com>
2024-08-06 00:20:07 +0200

    Tableviewcell works

---
+++ WatchChainIOS/Cell/FirebaseCell/MessageCell.swift
@@ -0,0 +1,23 @@
+//
+//  MessageCell.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 05/08/2024.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
