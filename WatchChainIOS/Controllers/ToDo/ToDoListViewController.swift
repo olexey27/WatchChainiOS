65cb735d388035a82f359c9dd643281b14280862
olexey27 <allxyog@gmail.com>
2024-10-23 22:40:21 +0200

    Persistent Local Data Storage Using UserDefaults

---
+++ WatchChainIOS/Controllers/ToDo/ToDoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ToDoController.swift
+//  WatchChainIOS
+//
+//  Created by Alexej K on 10/22/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
